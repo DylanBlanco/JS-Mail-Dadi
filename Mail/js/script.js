2dc8cbae616d4285d7583a1571efac22aa4581fe
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-17 02:49:15 +0200

    mail - creato evento submit

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,22 +1,34 @@
-//chiedi nome utente
-const mail =  prompt(`Inserisci la tua mail`);
-console.log (`La mail dell'utente è:`, mail);
+//Seleziona form
+const infoForm = document.querySelector (`form`);
 
-//array - lista autorizzati
-const autorizzati = ["dylan", "mj", "camilla"];
-console.log (`Mail autorizzate:`, autorizzati);
+//evento submit
+infoForm.addEventListener (`submit`,
+    function() {
+        //prendi e salva valore username
+        const mailInput = document.getElementById (`mail`);
+        const mail = mailInput.value;
+        console.log('mail:', mail, typeof mail);
 
-//condizione di verità
-if (mail == autorizzati[0]) {
-    console.log (`Questa mail è autorizzata:`, mail);
-}
-else if (mail == autorizzati[1]) {
-    console.log (`questa mail è autorizzata:`, mail);
-}
-else if (mail == autorizzati[2]) {
-    console.log (`questa mail è autorizzata:`, mail);
-}
-else {
-    console.log (`questa mail non è autorizzata:`, mail);
-    alert (`mail non autorizzata`);
-}
+        //array - lista autorizzati
+        const autorizzati = ["dylan@gmail.com", "mj@gmail.com", "camilla@gmail.com"];
+        console.log (`Mail autorizzate:`, autorizzati);
+
+        //condizione di verità
+        if (mail == autorizzati[0]) {
+            console.log (`Questa mail è autorizzata:`, mail);
+            alert (`mail autorizzata`);
+        }
+        else if (mail == autorizzati[1]) {
+            console.log (`questa mail è autorizzata:`, mail);
+            alert (`mail autorizzata`);
+        }
+        else if (mail == autorizzati[2]) {
+            console.log (`questa mail è autorizzata:`, mail);
+            alert (`mail autorizzata`);
+        }
+        else {
+            console.log (`questa mail non è autorizzata:`, mail);
+            alert (`mail non autorizzata`);
+        }
+    }
+)
