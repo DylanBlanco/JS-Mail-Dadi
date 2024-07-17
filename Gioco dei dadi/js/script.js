4b6f52797021dc96e95a19771bfdc6fc4317d482
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-17 03:51:07 +0200

    gioco dei dadi - genera num casuale da 1 a 6

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,3 +1,7 @@
 //chiedi numero e salva valore
 let numUser = prompt (`inserisci un numero da 1 a 6`);
 console.log (`numero inserito:`, numUser);
+
+//genera num. random da 1 a 6
+let casual = Math.floor(Math.random() * 6) + 1;
+console.log (`num. casuale generato:`, casual);
