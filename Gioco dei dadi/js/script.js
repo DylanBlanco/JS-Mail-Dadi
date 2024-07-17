d933d92a3ae20bd9a1aeb25c4c5d782b0b0a2c95
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-17 03:51:54 +0200

    gioco dei dadi - condizione di verità

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -5,3 +5,13 @@ console.log (`numero inserito:`, numUser);
 //genera num. random da 1 a 6
 let casual = Math.floor(Math.random() * 6) + 1;
 console.log (`num. casuale generato:`, casual);
+
+//condizione di verità
+if (numUser > casual) {
+    console.log (`Hai vinto`);
+    alert (`Hai vinto`);
+}
+else {
+    console.log (`Hai perso`);
+    alert (`Hai perso`);
+}
